e20aa9d1ed3e443a748f062000a3634356297ffe
Gabriel Nattis <gabrielbits@live.com>
2026-08-12 16:37:29 -0300

    Solved typos and added missing cleanup steps

--- install.sh
+++ install.sh
@@ -26,12 +26,11 @@ fi
 echo
 echo "============= Installing Spotify =============="
 echo     "Cloning and navigating to repo"
-git clone https://github.com/aome510/spotify-player
-cd spotify_player
-echo   "Installing spotify_player into the system"
-cargo install spotify_player --locked
-cd ..
-sudo rm -rf spotify_player
+wget https://github.com/aome510/spotify-player/releases/latest/download/spotify_player-x86_64-unknown-linux-gnu.tar.gz
+sudo tar -xvf spotify_player-x86_64-unknown-linux-gnu.tar.gz
+sudo mkdir /opt/spotify_player
+sudo cp -rf spotify_player /opt/spotify_player
+rm -rf spotify_player-x86_64-unknown-linux-gnu.tar.gz spotify_player
 echo "================== Done :) ===================="
 echo
 
@@ -49,7 +48,8 @@ echo
 echo "============= Installing Telegram ============="
 wget https://telegram.org/dl/desktop/linux
 sudo tar -xvf linux
-sudo cp -rf Telegram/ /opt/telegram/
+sudo cp -rf Telegram/* /opt/telegram/
+rm -rf Telegram linux
 echo "================== Done :) ===================="
 echo
 
