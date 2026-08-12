d74352b7557b0e400284cb37bb24140b2bf40760
Gabriel Nattis <gabrielbits@live.com>
2026-08-12 01:09:17 -0300

    Updated the scripts to the correct place

--- place.sh
+++ place.sh
@@ -1,27 +1,3 @@
-# Install necessary packages on my favourite distros
-packages="eza emacs kitty rust cargo python3 openssl-devel alsa-lib-devel libdbus-devel"
-file="/etc/os-release"
-
-if [ -f "$file" ]
-then
-    echo "$file found."
-    . $file
-
-    echo "Distro ID =" $ID
-    if [ "$ID" = "fedora" ]
-    then
-	sudo dnf install -y $packages
-    elif [ "$ID" = "arch" ] ||  [ "$ID" = "cachyos" ]
-    then
-	sudo pacman -S $packages	
-    elif [ "$ID" = "debian" ]
-    then
-	sudo apt get $packages
-    fi
-else
-    echo "$file not found."
-fi
-
 echo "===== Copying files to the right place... ====="
 echo "emacs/.emacs ~/"
 echo "bash/.bashrc ~/"
