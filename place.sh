d47b59f05a74946d650b7520560519e83f1ae5d0
Gabriel Nattis <gabrielbits@live.com>
2026-08-26 18:08:09 -0300

    Solved mkdir issue

--- place.sh
+++ place.sh
@@ -1,20 +1,22 @@
+echo "=========== Extracting KDE configs ============"
+python3 -m venv konsave
+source konsave/bin/activate
+pip install konsave
+konsave -i kde/KDE.knsv
+konsave -a KDE
+deactivate
+sudo rm -rf konsave
+echo "=================== Done ======================"
+
 echo "===== Copying files to the right place... ====="
 echo "emacs/.emacs ~/"
 echo "bash/.bashrc ~/"
 echo "kitty/kitty.conf ~/.config/kitty/"
 echo "wallpapers/* ~/Wallpapers"
 
-mkdir ~/Wallpapers
+mkdir -p ~/Wallpapers
 cp -r wallpapers/* ~/Wallpapers
 cp -r emacs/.emacs ~/
 cp -r bash/.bashrc ~/
 cp -r kitty/kitty.conf ~/.config/kitty/
-
-echo "=========== Extracting KDE configs ============"
-python3 -m venv konsave
-source konsave/bin/activate
-pip install konsave
-konsave -i kde/KDE.knsv
-deactivate
-sudo rm -rf konsave
 echo "=================== Done ======================"
